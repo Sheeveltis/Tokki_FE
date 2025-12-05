6a3edf18ca347d52592985cd5e88e7bc51f35f3c
ngdungteddy <nguyentrungdung1478@gmail.com>
2025-12-05 21:12:36 +0700

    feat: tạo trang quản lí cài đặt

---
+++ packages/app/features/admin/screens/Settings/index.jsx
@@ -0,0 +1,22 @@
+'use client'
+
+import React from 'react'
+import { Card, Space, Typography } from 'antd'
+
+const { Title, Text } = Typography
+
+export function Settings() {
+  return (
+    <Space direction="vertical" size="large" style={{ width: '100%' }}>
+      <Card>
+        <Title level={4} style={{ fontFamily: 'Arimo, sans-serif' }}>
+          Cài đặt
+        </Title>
+        <Text type="secondary">Hiện chưa có tuỳ chọn thay đổi theme.</Text>
+      </Card>
+    </Space>
+  )
+}
+
+export default Settings
+
