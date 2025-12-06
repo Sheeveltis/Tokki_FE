a093b0a9a73bae3d28fc310dc08aaab4f17c568e
khoav <khoavnase183164@fpt.edu.vn>
2025-12-06 15:34:55 +0700

    feat: init endpoints

---
+++ packages/app/provider/api/endpoints.js
@@ -0,0 +1,12 @@
+const DOMAIN = 'http://localhost:5031'
+const PREFIX = '/api'
+export const API_BASE_URL = `${DOMAIN}${PREFIX}`
+export const ENDPOINTS = {
+    BLOG: {
+      GET_ALL: '/Blog',              
+      GET_BY_ID: (id) => `/Blog/${id}`,
+      CREATE: '/Blog',                // POST: Tạo mới
+      UPDATE: (id) => `/Blog/${id}`,    
+      DELETE: (id) => `/Blog/${id}`,     
+    },
+  }
