7f1a15667976f3fe9af884e0900752a51472bcd5
PhamQuy31211 <quypp0312@gmail.com>
2025-12-08 18:20:51 +0700

    chore : change folder of payment

--- apps/web/app/(public)/payment/premium/page.jsx
+++ apps/web/app/(public)/payment/premium/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react'
-import { PremiumScreen } from 'app/features/payment/payment-package/premium-screen'
+import { PremiumScreen } from 'app/features/payment/premium-package/premium-screen'
 
 export default function PackagePremiumPage() {
   return <PremiumScreen />
