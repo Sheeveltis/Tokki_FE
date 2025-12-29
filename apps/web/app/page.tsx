ac671508b1ee50bf88a6726830d486d8318175d8
Sheeveltis <khoavo091003@gmail.com>
2025-12-29 14:21:27 +0700

    fix: has an invalid "default" export.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,23 @@
 'use client'
 
-export { HomeScreen as default } from '@tokki/app/features/home/screen'
+import { useRouter } from 'next/navigation'
+import dynamic from 'next/dynamic'
+
+const HomeScreen = dynamic(
+  () => import('@tokki/app/features/home/screen').then(mod => mod.HomeScreen),
+  { ssr: false }
+)
+
+export default function Page() {
+  const router = useRouter()
+
+  return (
+    <HomeScreen 
+      onHomePress={() => router.push('/')}
+      onRoadmapPress={() => router.push('/roadmap')}
+      onFlashcardPress={() => router.push('/flashcard')}
+      onBlogPress={() => router.push('/blog')}
+      onProfilePress={() => router.push('/profile')}
+    />
+  )
+}
