960ea0178b3588c475ff1b9b518e448acc9dc8cb
PhamQuy31211 <quypp0312@gmail.com>
2026-01-01 20:24:30 +0700

    chore : change import

--- packages/app/features/vocabulary/screens/VocabularyDetail/components/vocabulary-info-card.jsx
+++ packages/app/features/vocabulary/screens/VocabularyDetail/components/vocabulary-info-card.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'solito/navigation'
 import { Card, Descriptions, Image, Tag, Space, Button, Table } from 'antd'
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
 import { fetchUserDetail } from 'app/features/admin/screens/UserDetail/api/api'
