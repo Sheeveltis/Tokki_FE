146690a99d98ed40e1321588a10f8ed33c4af612
ngdungteddy <nguyentrungdung1478@gmail.com>
2026-02-07 12:04:24 +0700

    feat: add item screen roadmap

---
+++ packages/app/features/roadmap/screens/roadmap-tips-screen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { RoadmapTipsLayout } from '../components/roadmap-tips/roadmap-tips-layout.web'
+
+// Trang blog hướng dẫn cách học theo lộ trình TOPIK
+export function RoadmapTipsScreen({ tipId }) {
+  return <RoadmapTipsLayout tipId={tipId} />
+}
+
+
