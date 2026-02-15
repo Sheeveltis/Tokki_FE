941c9b7014a51ee70b0bfcaf4192bfaa44f8ced4
PhamQuy31211 <quypp0312@gmail.com>
2026-02-15 11:41:22 +0700

    feat : add screen wordle play

---
+++ packages/app/features/minigame/screens/wordle/wordle-play-screen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import WordlePlayWeb from '../../components/wordle/wordle-play/wordle-play.web'
+
+export function WordlePlayScreen() {
+  return <WordlePlayWeb />
+}
+
+export default WordlePlayScreen
+
+
+
