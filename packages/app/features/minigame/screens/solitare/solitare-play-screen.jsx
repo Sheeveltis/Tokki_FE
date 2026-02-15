a9db2073dae45de26fbbe1441a81b5da536c6bc0
Sheeveltis <khoavo091003@gmail.com>
2026-02-15 15:31:10 +0700

    fix: wrong import path

--- packages/app/features/minigame/screens/solitare/solitare-play-screen.jsx
+++ packages/app/features/minigame/screens/solitare/solitare-play-screen.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { SolitarePlayLayoutWeb } from '../../components/solitare/solitare-play/solitare-play-native/solitare-play-layout.web'
+import SolitarePlayLayoutWeb from '../../components/solitare/solitare-play/solitare-play-web/solitare-play-web'
 
 /**
  * Screen wrapper cho màn chơi solitaire (web).
